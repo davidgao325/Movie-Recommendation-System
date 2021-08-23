7359d70ba4176db5ee905b48901fa8e04ad73743
davidgao325 <50885792+davidgao325@users.noreply.github.com>
2021-08-23 13:23:47 -0400

    Add files via upload

---
+++ cleaning.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug 14 15:49:45 2021
+
+@author: david
+"""
+import pandas as pd
+
+df = pd.read_csv('movies.csv')
+
+df['genres'] = df['genres'].str.replace(',','')
+df['us_grossMillions'] = df['us_grossMillions'].str.replace('$','')
+df['us_grossMillions'] = df['us_grossMillions'].str.replace('M','')
+df['votes'] = df['votes'].str.replace(',','')
+df['votes'] = df['votes'].astype(int)
+df['year'] = df['year'].str.extract('(\d+)').astype(int)
+df['timeMin'] = df['timeMin'].str.replace('min','')
+df['timeMin'] = df['timeMin'].astype(int)
+df['us_grossMillions'] = pd.to_numeric(df['us_grossMillions'], errors='coerce')
+df.to_csv("cleaned_data.csv")
